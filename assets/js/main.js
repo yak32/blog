7c151f7c00f161eb91fda4118dae1619e60615c5
Iakov Sumygin <sumygin@gmail.com>
2017-04-21 23:40:23 +0100

    add sidebar to appear on macbooks

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
 	skel.breakpoints({
 		xlarge:	'(max-width: 1680px)',
 		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
+		medium:	'(max-width: 880px)',
 		small:	'(max-width: 736px)',
 		xsmall:	'(max-width: 480px)'
 	});
