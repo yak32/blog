4d2f2be95fbd70bf98f86731023dee1d83adf79d
Iakov Sumygin <sumygin@gmail.com>
2017-04-22 01:03:33 +0100

    restore sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
 	skel.breakpoints({
 		xlarge:	'(max-width: 1680px)',
 		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 880px)',
+		medium:	'(max-width: 980px)',
 		small:	'(max-width: 736px)',
 		xsmall:	'(max-width: 480px)'
 	});
